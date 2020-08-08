891620d86c387fd90f8f87f5a2b7e16b256009d1
Kevin Hong <kevink556@gmail.com>
2020-08-08 14:41:39 +0800

    update sql_queries

--- sql_queries.py
+++ sql_queries.py
@@ -106,7 +106,7 @@ artist_table_create = ("""
         artist_id   VARCHAR     NOT NULL,
         name        VARCHAR     NOT NULL,
         location    VARCHAR,
-        latitude   VARCHAR,
+        latitude    VARCHAR,
         longitude   VARCHAR)
     DISTKEY(artist_id)
     SORTKEY(artist_id)
@@ -153,14 +153,18 @@ songplay_table_insert = ("""
                 user_id, session_id, level, location, user_agent
            FROM (SELECT songplay_id,
                         TIMESTAMP 'epoch' +  ts/1000.0 * INTERVAL '1 seconds' AS start_time, 
-                        song,
+                        song, length, artist
                         user_id, session_id, level, location, user_agent
                    FROM staging_events
-                   WHERE page = 'NextSong') AS u
+                  WHERE page = 'NextSong') AS u
            JOIN (SELECT song_id, artist_id,
-                        title AS song   
+                        title AS song,
+                        duration AS length,
+                        artist_name AS artist
                    FROM staging_songs) AS s
              ON u.song = s.song 
+            AND u.length = s.length
+            AND u.artist = s.artist
 """)
 
 user_table_insert = ("""
@@ -261,11 +265,3 @@ insert_table_queries = {
     "artist_table_insert": artist_table_insert,
     "time_table_insert": time_table_insert
 }
-
-
-
-
-# create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-# drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-# copy_table_queries = [staging_events_copy, staging_songs_copy]
-# insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
